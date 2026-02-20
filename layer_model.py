813eeaf58192bebfde1379b0c27c1cfb5471758c
szk <sawyer.kaplan@gmail.com>
2026-02-19 19:54:49 -0800

    clarity

--- layer_model.py
+++ layer_model.py
@@ -2,9 +2,9 @@ import numpy as np
 
 # Layer model
 def layer_model(n):
-    Ta = 255 # above atmo temp in K
+    Ta = 255 # above atmo temp in K 
     Ts = [0] * n # init list of surface temperatures
     for i in range(n):
-        Ts[i] = Ta * (1.19) ** (i + 1) # Surface temperature for n layers
+        Ts[i] = Ta * (1.19) ** (i + 1) # temp of n bb layers add i + 1 bc i starts at 0
     return(Ts)
 print(layer_model(4))
